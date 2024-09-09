b648003f468397c6b00c023e464b80c8b8316fb3
Vaishugaikwad <gaikwadvaishnavi226@gmail.com>
2024-09-09 12:19:28 +0530

    first commit

---
+++ while5/program8.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	int product = 1;
+	int i = 10;
+	while(i>=1) {
+	
+		if(i%2==1) {
+		product *= i;
+		}
+	i--;
+	}
+	print(product);
+}
---
+++ while5/program9.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	int num = 7;
+	while(num>=0) {
+	
+		if(num==0) {
+		print("0 Days Assignment is overdue");
+		break;
+		}
+	print("$num days remaining");
+	num--;
+	}
+}
